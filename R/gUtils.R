583532969771267ac290e2a1561d369274ca6dba
arthurgc-sci <arthur.gairincalvo@gmail.com>
2025-04-30 19:03:54 +0200

    Update gUtils.R

--- R/gUtils.R
+++ R/gUtils.R
@@ -1,6 +1,5 @@
 #utils related to insect leg analysis pipeline developped here mostly by Arthur Gairin-Calvo but also Laurent Gilquin
 #---------------------------------------------------------------------------------------------------
-importFrom(magrittr, `%>%`)
 
 #' Correct illumination
 #'
